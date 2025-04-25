6bbac6e793a4880c19c0e1a58d7cbc0fd1d85b23
joaquim <mjoaquim12@gmail.com>
2025-04-25 23:53:04 +0400

    Add asto island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://jstar-astro.netlify.app"
-  });
+  site: "https://jstar-astro.netlify.app",
+  integrations: [preact()]
+});
